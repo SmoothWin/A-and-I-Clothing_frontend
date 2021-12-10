9bcce3e7e6f60efec477c6a7be4b8a0c646d6f4c
Christian Chitanu <christian.chitanu@hotmail.com>
2021-12-10 03:29:01 -0500

    more changes and quality life changes

--- pages/bigorder.js
+++ pages/bigorder.js
@@ -6,6 +6,7 @@ import BootstrapJS from '../components/Bootstrap'
 
 export default function bigorder(){
     const [selectedFile, setSelectedFile] = useState(null);
+    const [message, setMessage] = useState(null)
 
     function onChangeHandler(e){
         setSelectedFile(e.target.files[0])
@@ -14,9 +15,11 @@ export default function bigorder(){
         const data = new FormData() 
         data.append('file', selectedFile)
         try{
-            await axios.post("http://localhost:8000/bigorders/upload", data,{ 
+            let response = await axios.post("http://localhost:8000/bigorders/upload", data,{ 
                 // receive two    parameter endpoint url ,form data
             })
+            console.log(response.data.message)
+            setMessage(response.data.message)
         }catch(e){
             console.log(e)
         }
@@ -29,28 +32,29 @@ export default function bigorder(){
         
         <h2 className="display-4 col-10 mx-5 text-center text-sm-start">Special orders for Businesses</h2>
         <div className="container-sm d-flex flex-column flex-md-row">
-            <div className="col-12 col-md-6">
-                <p className="pt-3">Steps for orders:</p>
-                <p className="pt-4">1- Download the form and fill it out.</p>
-                <p className="pt-4">2- Upload the form below and click Send Order.</p>
-                <p className="pt-4">3- A business associate will contact you ASAP to give you a quote.</p>
-                <p className="pt-4">4- Once everything is in order, we will prepare your order and bring it to you!</p>
+            <div className="col-12 col-md-6 mx-1">
+                <p className="text-secondary pt-3 fs-5">Steps for orders:</p>
+                <p className="text-secondary pt-4 fs-5">1- Download the form and fill it out.</p>
+                <p className="text-secondary pt-4 fs-5">2- Upload the form below and click Send Order.</p>
+                <p className="text-secondary pt-4 fs-5">3- A business associate will contact you ASAP to give you a quote.</p>
+                <p className="text-secondary pt-4 fs-5">4- Once everything is in order, we will prepare your order and bring it to you!</p>
                 
             </div>
-                <img className="img-thumbnail h-75 mx-auto" height={300} src="/image.png"/>
-            </div>
+                <img className="img-thumbnail h-25 w-50 mt-auto mb-auto mx-auto" src="/image.png"/>
+        </div>
             
-                <div className="row justify-content-around mx-5 pt-4">
-                    <a className="btn btn-danger btn-lg" href="/bigorder_template.csv" download> Download Form</a>
-                    <div className="form-group">
-                        <input className="form-control" type="file" accept=".csv"
-                        onChange={onChangeHandler}
-                        />
-                    </div>
-                </div>
-                <div className="row">
-                    <button type="button" className="btn btn-danger btn-lg" onClick={onClickHandler}>Send Order</button>
-                </div>
+                    
+        <div className="col-12 col-sm-8 col-md-10 col-lg-8 d-flex flex-column mx-auto justify-content-around">
+            <a className="col-12 mt-3 btn btn-danger btn-lg" href="/bigorder_template.csv" download> Download Form</a>
+            <div className="col-12 mt-3 form-group">
+                <input className="form-control" type="file" accept=".csv"
+                onChange={onChangeHandler}
+                />
+            </div>
+            <button type="button" className="col-12 mt-3 btn btn-danger btn-lg" onClick={onClickHandler}>Send Order</button>
+        </div>
+
+        <div className="mx-auto mt-3 text-center text-success">{message}</div>
             
         </>
     )
