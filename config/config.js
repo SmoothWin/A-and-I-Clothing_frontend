49f118df6bedbccf5493d949306d9094e862d255
Christian Chitanu <christian.chitanu@hotmail.com>
2022-02-14 18:58:44 -0500

    fixed production bug

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 const url = "https://aiclothin-b.herokuapp.com"
 const urlTest = "http://localhost:8000"
 
-export default (process.env.ENDTEST == "true")?urlTest:urlTest
+export default (process.env.ENDTEST = "true")?urlTest:url
